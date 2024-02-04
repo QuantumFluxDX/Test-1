ac18b6185b2b217522bd531950cb4826d7d55d0a
QuantumFluxDX <pratikd2003@gmail.com>
2024-02-04 14:30:06 +0530

    Refactored and Added functions Added heatmaps for 5s, 3s and 1s delays

---
+++ functions.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+
+def extract_measurements(filenames, filter_params):
+    """Creates a single dataframe from a list of csv files"""
+    
+    # Initialise the dataframe
+    dataframe = []
+    for file in filenames:
+        # Read data from a csv file in filenames
+        data = pd.read_csv(file) 
+
+        # Drop the rows parameter values = '-'
+        for param in filter_params:
+            data = data[data[param] != '-']
+
+        # Convert parameter values to float
+        for param in filter_params:
+            data[param] = data[param].astype('float')
+
+        # Only keep rows with 5G Network Mode
+        data = data[data['NetworkMode'] == '5G']
+
+        # Append data to the dataframe
+        dataframe.append(data)
+
+    # Concatanate the datas in dataframe
+    dataframe = pd.concat(dataframe).reset_index(drop=True)
+    return dataframe[filter_params]
+
+def scale(measurements, filter_params):
+    scaled = measurements.copy()
+    for param in filter_params:
+        L = _scaling(measurements[param])
+        scaled[param] = L
+    return scaled
+
+def _scaling(list):
+    min_list = min(list)
+    max_list = max(list)
+    
+    scaled_list = []
+    for element in list:
+        scaled_element = (element - min_list)/(max_list - min_list)
+        scaled_list.append(scaled_element)
+    return scaled_list
+
+def add_delay(measurements, delay):
+    if delay != 0:
+        scaled_present = measurements[:-delay]
+        scaled_future = measurements[delay:]
+    else:
+        scaled_future = measurements
+        scaled_present = measurements
+
+    return [scaled_future, scaled_present]
+
+def create_heatmap(future_values, present_values, filter_params):
+    heatmap = []
+    for i in range(len(filter_params)):
+        heatmap.append([])
+        for j in range(len(filter_params)):
+            param_1 = future_values[filter_params[i]]
+            param_2 = present_values[filter_params[j]]
+            corr_coeff = np.corrcoef(param_1, param_2)
+            heatmap[i].append(corr_coeff[0][1])
+
+    return heatmap
---
+++ main.py
@@ -0,0 +1,72 @@
+#%%
+import matplotlib.pyplot as plt
+
+from functions import *
+#%%
+
+filepath = 'irish_dataset/'
+filenames = []
+for num in range(1,15):
+    filename = f"{filepath}DD{num}.csv"
+    filenames.append(filename)
+
+filter_params = ['Speed', 'RSRP', 'RSRQ', 'SNR', 'CQI', 'RSSI', 'DL_bitrate'
+                 ,'NRxRSRP']
+
+#%%
+
+measurements = extract_measurements(filenames, filter_params)
+scaled = scale(measurements, filter_params)
+[future_5s, present_5s] = add_delay(scaled, 5)
+[future_3s, present_3s] = add_delay(scaled, 3)
+[future_1s, present_1s] = add_delay(scaled, 1)
+
+heatmap_5s = create_heatmap(future_5s, present_5s, filter_params)
+heatmap_3s = create_heatmap(future_3s, present_3s, filter_params)
+heatmap_1s = create_heatmap(future_1s, present_1s, filter_params)
+
+#%%
+
+# plt.plot(measurements['DL_bitrate'])
+# plt.title("Throughput")
+
+#%%
+#%%
+plt.figure()
+plt.imshow(heatmap_5s,cmap='jet')
+plt.xticks(list(range(len(filter_params))),filter_params,rotation=45)
+plt.yticks(list(range(len(filter_params))),filter_params)
+plt.xlabel('Present')
+plt.ylabel('Future')
+plt.colorbar()
+for i in range(len(filter_params)):
+    for j in range(len(filter_params)):
+        text = plt.text(j, i, f'{heatmap_5s[i][j]:.2f}', ha='center',
+                         va='center', color='k')
+plt.savefig("heatmaps/heatmap_irish_5s.png")
+
+plt.figure()
+plt.imshow(heatmap_3s,cmap='jet')
+plt.xticks(list(range(len(filter_params))),filter_params,rotation=45)
+plt.yticks(list(range(len(filter_params))),filter_params)
+plt.xlabel('Present')
+plt.ylabel('Future')
+plt.colorbar()
+for i in range(len(filter_params)):
+    for j in range(len(filter_params)):
+        text = plt.text(j, i, f'{heatmap_3s[i][j]:.2f}', ha='center',
+                         va='center', color='k')
+plt.savefig("heatmaps/heatmap_irish_3s.png")
+
+plt.figure()
+plt.imshow(heatmap_1s,cmap='jet')
+plt.xticks(list(range(len(filter_params))),filter_params,rotation=45)
+plt.yticks(list(range(len(filter_params))),filter_params)
+plt.xlabel('Present')
+plt.ylabel('Future')
+plt.colorbar()
+for i in range(len(filter_params)):
+    for j in range(len(filter_params)):
+        text = plt.text(j, i, f'{heatmap_1s[i][j]:.2f}', ha='center',
+                         va='center', color='k')
+plt.savefig("heatmaps/heatmap_irish_1s.png")
